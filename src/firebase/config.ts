c112796e15e1b0bffa09ab1566d35fef615e637d
Libaan Ibrahim <liibaanzaki5@gmail.com>
2026-05-26 18:51:57 +0000

    Just replace the existing firebase keys and admin SDK json:
    const fireb

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,11 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBnpvqipkjZf5PpmvtLXbVzKEWL5FYadVM",
-  authDomain: "connectnexus-a9acf.firebaseapp.com",
-  databaseURL: "https://connectnexus-a9acf-default-rtdb.firebaseio.com",
-  projectId: "connectnexus-a9acf",
-  storageBucket: "connectnexus-a9acf.firebasestorage.app",
-  messagingSenderId: "903693576297",
-  appId: "1:903693576297:web:5d40ad8048f211db449af8"
+  apiKey: "AIzaSyAvSrhpON5ZQSsecrQTOZ44dlI7heclpXc",
+  authDomain: "oskarshop-631c5.firebaseapp.com",
+  databaseURL: "https://oskarshop-631c5-default-rtdb.firebaseio.com",
+  projectId: "oskarshop-631c5",
+  storageBucket: "oskarshop-631c5.firebasestorage.app",
+  messagingSenderId: "30485488082",
+  appId: "1:30485488082:web:8fbf7bdd72ff519ebd680b",
+  measurementId: "G-XZBH0GH7WC"
 };
